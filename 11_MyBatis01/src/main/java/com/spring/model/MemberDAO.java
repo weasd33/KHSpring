2b4591c424cbefe916a479ee22a75024c03d57c7
SHIM <weasd33@naver.com>
2022-05-25 14:50:11 +0900

    Spring - myBatis 설정

---
+++ 11_MyBatis01/src/main/java/com/spring/model/MemberDAO.java
@@ -0,0 +1,20 @@
+package com.spring.model;
+
+import java.util.List;
+
+public interface MemberDAO {
+	
+	List<MemberDTO> getMemberList();
+	
+	int insertMember(MemberDTO dto);
+	
+	MemberDTO getMember(int num);
+	
+	int updateMember(MemberDTO dto);
+	
+	int deleteMember(int num);
+	
+	void updateSequence(int num);
+	
+	List<MemberDTO> searchMemberList(String field, String keyword);
+}
---
+++ 11_MyBatis01/src/main/java/com/spring/model/MemberDAOImpl.java
@@ -0,0 +1,50 @@
+package com.spring.model;
+
+import java.util.List;
+
+public class MemberDAOImpl implements MemberDAO {
+
+	@Override
+	public List<MemberDTO> getMemberList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertMember(MemberDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public MemberDTO getMember(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateMember(MemberDTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteMember(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void updateSequence(int num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<MemberDTO> searchMemberList(String field, String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
